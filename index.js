69a7084cadb8d3463f3fd0234a0f2c9fdc217b5b
minjanupriya <134762713+minjanupriya@users.noreply.github.com>
2023-06-12 00:28:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -322,98 +322,98 @@ $(document).ready(function () {
 
 
 /** Launches payment request flow when user taps on buy button. */
-function onOrderNow() {
-  if (!window.PaymentRequest) {
-    console.log('Web payments are not supported in this browser.');
-    return;
-  }
+// function onOrderNow() {
+//   if (!window.PaymentRequest) {
+//     console.log('Web payments are not supported in this browser.');
+//     return;
+//   }
 
-  // Create supported payment method.
-  const supportedInstruments = [
-    {
-      supportedMethods: ['https://tez.google.com/pay'],
-      data: {
-        pa: 'merchant-vpa@xxx',
-        pn: 'Merchant Name',
-        tr: '1234ABCD',  // Your custom transaction reference ID
-        url: 'https://url/of/the/order/in/your/website',
-        mc: '1234', //Your merchant category code
-        tn: 'Purchase in Merchant',
-      },
-    }
-  ];
+//   // Create supported payment method.
+//   const supportedInstruments = [
+//     {
+//       supportedMethods: ['https://tez.google.com/pay'],
+//       data: {
+//         pa: 'merchant-vpa@xxx',
+//         pn: 'Merchant Name',
+//         tr: '1234ABCD',  // Your custom transaction reference ID
+//         url: 'https://url/of/the/order/in/your/website',
+//         mc: '1234', //Your merchant category code
+//         tn: 'Purchase in Merchant',
+//       },
+//     }
+//   ];
 
-  // Create order detail data.
-  const details = {
-    total: {
-      label: 'Total',
-      amount: {
-        currency: 'INR',
-        value: '10.01', // sample amount
-      },
-    },
-    displayItems: [{
-      label: 'Original Amount',
-      amount: {
-        currency: 'INR',
-        value: '10.01',
-      },
-    }],
-  };
+//   // Create order detail data.
+//   const details = {
+//     total: {
+//       label: 'Total',
+//       amount: {
+//         currency: 'INR',
+//         value: '10.01', // sample amount
+//       },
+//     },
+//     displayItems: [{
+//       label: 'Original Amount',
+//       amount: {
+//         currency: 'INR',
+//         value: '10.01',
+//       },
+//     }],
+//   };
 
-  // Create payment request object.
-  let request = null;
-  try {
-    request = new PaymentRequest(supportedInstruments, details);
-  } catch (e) {
-    console.log('Payment Request Error: ' + e.message);
-    return;
-  }
-  if (!request) {
-    console.log('Web payments are not supported in this browser.');
-    return;
-  }
+//   // Create payment request object.
+//   let request = null;
+//   try {
+//     request = new PaymentRequest(supportedInstruments, details);
+//   } catch (e) {
+//     console.log('Payment Request Error: ' + e.message);
+//     return;
+//   }
+//   if (!request) {
+//     console.log('Web payments are not supported in this browser.');
+//     return;
+//   }
 
-  var canMakePaymentPromise = checkCanMakePayment(request);
-  canMakePaymentPromise
-    .then((result) => {
-      showPaymentUI(request, result);
-    })
-    .catch((err) => {
-      console.log('Error calling checkCanMakePayment: ' + err);
-    });
-}
+//   var canMakePaymentPromise = checkCanMakePayment(request);
+//   canMakePaymentPromise
+//     .then((result) => {
+//       showPaymentUI(request, result);
+//     })
+//     .catch((err) => {
+//       console.log('Error calling checkCanMakePayment: ' + err);
+//     });
+// }
 
-// Show the payment request UI.
-function showPaymentUI(request, canMakePayment) {
-  if (!canMakePayment) {
-    handleNotReadyToPay();
-    return;
-  }
+// // Show the payment request UI.
+// function showPaymentUI(request, canMakePayment) {
+//   if (!canMakePayment) {
+//     handleNotReadyToPay();
+//     return;
+//   }
 
-  // Set payment timeout.
-  let paymentTimeout = window.setTimeout(function() {
-    window.clearTimeout(paymentTimeout);
-    request.abort()
-      .then(function() {
-        console.log('Payment timed out after 20 minutes.');
-      })
-      .catch(function() {
-        console.log('Unable to abort, user is in the process of paying.');
-      });
-  }, 20 * 60 * 1000); /* 20 minutes */
+//   // Set payment timeout.
+//   let paymentTimeout = window.setTimeout(function() {
+//     window.clearTimeout(paymentTimeout);
+//     request.abort()
+//       .then(function() {
+//         console.log('Payment timed out after 20 minutes.');
+//       })
+//       .catch(function() {
+//         console.log('Unable to abort, user is in the process of paying.');
+//       });
+//   }, 20 * 60 * 1000); /* 20 minutes */
 
-  request.show()
-    .then(function(instrument) {
-      window.clearTimeout(paymentTimeout);
-      processResponse(instrument); // Handle response from browser.
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
-}
+//   request.show()
+//     .then(function(instrument) {
+//       window.clearTimeout(paymentTimeout);
+//       processResponse(instrument); // Handle response from browser.
+//     })
+//     .catch(function(err) {
+//       console.log(err);
+//     });
+// }
 
-// Attach the onOrderNow function to the "Order Now" button click event
-$(".orderNowBtn").click(function () {
-  onOrderNow();
-});
+// // Attach the onOrderNow function to the "Order Now" button click event
+// $(".orderNowBtn").click(function () {
+//   onOrderNow();
+// });
